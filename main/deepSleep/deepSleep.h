29de65322e1a52d67e205d63a1caf7eb921f2fae
Piemarz <piemarz91@gmail.com>
2024-01-28 15:15:00 +0100

    moved modules into folders.

---
+++ main/deepSleep/deepSleep.h
@@ -0,0 +1,15 @@
+/*
+ * deepSleep.h
+ *
+ *  Created on: 28 Jan 2024
+ *      Author: pierl
+ */
+
+#ifndef MAIN_DEEPSLEEP_DEEPSLEEP_H_
+#define MAIN_DEEPSLEEP_DEEPSLEEP_H_
+
+void deepSleepInit(void);
+
+void deepSleepRun(void);
+
+#endif /* MAIN_DEEPSLEEP_DEEPSLEEP_H_ */
---
+++ main/main.h
@@ -0,0 +1,15 @@
+/*
+ * main.h
+ *
+ *  Created on: 28 Jan 2024
+ *      Author: pierl
+ */
+
+#ifndef MAIN_MAIN_H_
+#define MAIN_MAIN_H_
+
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#endif /* MAIN_MAIN_H_ */
